13b68d7125e8cd9c1836eef8295d1b6f2b944619
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-04-12 23:04:23 +0400

    Add favicons, meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import Intro from '@/widgets/intro/ui/intro'
 import HomepageWrapper from '@/widgets/home-page-wrapper'
+import Head from 'next/head'
 
-export default function Home() {
+export default function HomePage() {
   return (
     <HomepageWrapper>
+      <Head>
+        <title>Гитлер или Соловьев?</title>
+        <meta rel="description">Проверьте интуицию, сыграв в игру, где вам нужно угадать чья это цитата: Владимира Соловьева (журналист) или Адольфа Гитлера (политик)</meta>
+      </Head>
       <Intro />
     </HomepageWrapper>
   )
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,10 +1,15 @@
 import PlayPageWrapper from '@/widgets/play-page-wrapper'
 import Game from '@/widgets/game'
 import Disclaimer from '@/widgets/disclaimer'
+import Head from 'next/head'
 
 export default function PlayPage() {
   return (
     <PlayPageWrapper>
+      <Head>
+        <title>Играть — Гитлер или Соловьев?</title>
+        <meta rel="description">Проверьте интуицию, сыграв в игру, где вам нужно угадать чья это цитата: Владимира Соловьева (журналист) или Адольфа Гитлера (политик)</meta>
+      </Head>
       <Game />
       <Disclaimer />
     </PlayPageWrapper>
