14d6903ed9d3e2aed623bf130d698e26a4faf20c
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-04-12 17:40:45 +0400

    Quotes

--- src/features/questions/ui/question-body.tsx
+++ src/features/questions/ui/question-body.tsx
@@ -2,15 +2,17 @@ import React from 'react'
 import styles from './styles.module.scss'
 import { QuoteModel, HITLER, SOLOVYEV } from '@/features/questions/model'
 import { Person } from '@/entities/person'
+import { generateQuote } from '@/features/questions/lib/generate-quote'
+import cx from 'classnames'
 import Hitler from '@/assets/persons/Hitler.jpeg'
 import Solovyev from '@/assets/persons/Solovyev.jpeg'
 
 export function QuestionBody() {
-  const [quote, setQuote] = React.useState<null | QuoteModel>(null)
+  const [quote, setQuote] = React.useState<QuoteModel>(() => generateQuote())
   const [hoverOn, setHoverOn] = React.useState<null | typeof HITLER | typeof SOLOVYEV>(null)
 
   const handleAnswer = (answer: typeof HITLER | typeof SOLOVYEV) => () => {
-
+    setQuote(generateQuote())
   }
 
   return (
@@ -29,6 +31,10 @@ export function QuestionBody() {
         onHover={() => setHoverOn(SOLOVYEV)}
         onSelect={handleAnswer(SOLOVYEV)}
       />
+      <span className={cx(styles.quote, { 
+        [styles.slideLeft]: hoverOn === HITLER,
+        [styles.slideRight]: hoverOn === SOLOVYEV,
+      })}>{quote.quote}</span>
     </div>
   )
 }
---
+++ src/pages/api/reportProgress.ts
@@ -0,0 +1,25 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import * as Yup from 'yup'
+import { ru as yupRuLocale } from 'yup-locales'
+
+Yup.setLocale(yupRuLocale)
+
+const RequestSchema = Yup.object({
+  correct: Yup.number().min(0).integer().required(),
+  questions: Yup.number().min(1).integer().required()
+})
+
+type Response = { ok: true } | { ok: false, error: 'INVALID_DATA' }
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Response>
+) {
+  await RequestSchema.validate(req.body)
+  const body = req.body as Yup.InferType<typeof RequestSchema>
+  if(body.correct > body.questions) {
+    res.status(400).json({ ok: false, error: 'INVALID_DATA' })
+    return
+  }
+  res.status(200).json({ ok: true })
+}
