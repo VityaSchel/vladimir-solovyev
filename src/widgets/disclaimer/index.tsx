8dac23f08cf8901fdd4596642be6599fe1d56ebd
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-04-13 14:12:41 +0400

    Add link to github

--- src/widgets/disclaimer/index.tsx
+++ src/widgets/disclaimer/index.tsx
@@ -1,9 +1,12 @@
 import styles from './styles.module.scss'
+import Link from 'next/link'
 
 export default function Disclaimer() {
   return (
     <div className={styles.disclaimer}>
       <span>Автор сайта осуждает Адольфа Гитлера и фашистскую идеологию.</span>
+      {' '}
+      <Link href='https://github.com/VityaSchel/vladimir-solovyev/tree/new-react'>Код на GitHub</Link>
     </div>
   )
 }
