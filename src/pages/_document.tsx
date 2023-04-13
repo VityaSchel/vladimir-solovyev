a9a496193e98e10ccf5554d607c07e5e3a5bdfc5
Viktor <59040542+VityaSchel@users.noreply.github.com>
2023-04-13 13:59:26 +0400

    Netlify doesn't support script injection on SSR pages

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,6 +7,7 @@ export default function Document() {
         <meta name="description" content="Проверьте интуицию, сыграв в игру, где вам нужно угадать чья это цитата: Владимира Соловьева (журналист) или Адольфа Гитлера (политик)" />
         <meta name="keywords" content="Гитлер, Соловьев, игра" />
         <meta name="author" content="hloth" />
+        <meta name="yandex-verification" content="7d2f180609697e51" />
         <link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
